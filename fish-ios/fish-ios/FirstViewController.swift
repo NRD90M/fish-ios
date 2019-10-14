9acb8c98dcac516921e43c3c3a5681f02802acbe
caiwenshu <wenshu.cai@shenmajr.com>
2019-10-14 11:55:01 +0800

    项目初始化

---
+++ fish-ios/fish-ios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  fish-ios
+//
+//  Created by caiwenshu on 2019/10/14.
+//  Copyright © 2019 caiwenshu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ fish-ios/fish-ios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  fish-ios
+//
+//  Created by caiwenshu on 2019/10/14.
+//  Copyright © 2019 caiwenshu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
