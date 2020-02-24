920902441e531a91214e63794b39231da2096cec
caiwenshu <wenshu.cai@shenmajr.com>
2020-02-24 18:18:30 +0800

    wifi连接调研完成，等待页面进行完善

--- fish-ios/fish-ios/fish-ios/Main/Pond/SetupNet/SetupNetViewController.swift
+++ fish-ios/fish-ios/fish-ios/Main/Pond/SetupNet/SetupNetViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 import NetworkExtension //导入网络扩展框架
+import SystemConfiguration.CaptiveNetwork
 
 class SetupNetViewController: UIViewController {
 
+    var client: FishMainApi = FishMainApi()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +23,19 @@ class SetupNetViewController: UIViewController {
         
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "navigationbar_arrow_back"), style: .plain, target: self, action: #selector(dismissViewController(animated:)))
         
-        connectWifi()
+       
+        let wifiInfo = getWifiInfo()
+        NSLog("SSID(WiFi名称): \(wifiInfo.0)")
+        NSLog("BSSID(Mac地址): \(wifiInfo.1)")
+        let ssid = wifiInfo.0
+        
+        if ssid != "smart" {
+             connectWifi()
+        }
+        
+        
+//        scanWifiInfos()
+//        connectWifi()
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,7 +70,89 @@ class SetupNetViewController: UIViewController {
              UIApplication.shared.openURL(url! as URL)
         }
     }
+    
+    
+    @IBAction func netConfigButtonTouchUpInside(_ sender: Any) {
+        
+        sendWIFISSIDAndPassPhraseToDevice()
+        
+        //        self.delegate?.editTrigger(data:unpackedData)
+        
+    }
+    
+    
+    
+    func sendWIFISSIDAndPassPhraseToDevice() {
+        
+        let url = "http://192.168.12.1:9999/onekey-net-config"
+        
+        var params = [String:String]()
+        params["ssid"] = "HW"
+        params["psk"] = "11111111"
+        
+        self.client.getPath(url, parameters: params) { (data, error) in
+            print(data)
+        }
+        
+//        NEHotspotHelper.register(options: <#T##[String : NSObject]?#>, queue: <#T##DispatchQueue#>, handler: <#T##NEHotspotHelperHandler##NEHotspotHelperHandler##(NEHotspotHelperCommand) -> Void#>)
+    }
 
+    //获取 WiFi 信息
+    func getWifiInfo() -> (ssid: String, mac: String) {
+        if let cfas: NSArray = CNCopySupportedInterfaces() {
+            for cfa in cfas {
+                if let dict = CFBridgingRetain(CNCopyCurrentNetworkInfo(cfa as! CFString)) {
+                    if let ssid = dict["SSID"] as? String, let bssid = dict["BSSID"] as? String {
+                        return (ssid, bssid)
+                    }
+                }
+            }
+        }
+        return ("未知", "未知")
+    }
+//  https://www.jianshu.com/p/0119548fe386 需要主动申请故忽略该方法
+//    func scanWifiInfos() {
+//
+//        var options = [String: NSObject]()
+//        options[kNEHotspotHelperOptionDisplayName] = "Try Here" as NSObject?
+//
+//        NSLog("Lets register", "")
+//
+//        let queue = DispatchQueue.init(label: "Demo")
+//
+//        let returnType = NEHotspotHelper.register(options: options, queue: DispatchQueue.main) { (cmd) in
+//
+////            let netWork:NEHotspotNetwork
+//            NSLog("Returned", "")
+//
+//            print(cmd)
+//
+//            if cmd.commandType == NEHotspotHelperCommandType.evaluate || cmd.commandType ==  NEHotspotHelperCommandType.filterScanList {
+//
+//                for network in cmd.networkList! {
+//
+//                    let wifiInfoString = "SSID:" + network.ssid + " CommandType:" + String(Float((cmd.commandType).rawValue))
+//                    print(wifiInfoString)
+//
+//                    if network.ssid == "smart" {
+//
+//                        network.setConfidence(NEHotspotHelperConfidence.high)
+//                        let response = cmd.createResponse(NEHotspotHelperResult.success)
+//                        print(response)
+//
+//                        response.setNetworkList([network])
+//                        response.setNetwork(network)
+//                        response.deliver()
+//                    }
+//                }
+//            }
+//        }
+//
+//
+//        print(returnType)
+//    }
+    
+  
     
     /*
     // MARK: - Navigation
